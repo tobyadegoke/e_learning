569f2139f734db18053d825cb554917db1912b8b
Oluwatobi Adegoke <tobyadegoke@hotmail.com>
2018-11-01 18:09:21 +0000

    clean legacy code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,6 @@ import { VideoPlayer } from '@ionic-native/video-player';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { CreateQuizPage } from '../pages/create-quiz/create-quiz';
-import { RemoveForumPostPage } from '../pages/remove-forum-post/remove-forum-post';
-import { VideosPage } from '../pages/videos/videos';
-import { AddForumPostPage } from '../pages/add-forum-post/add-forum-post';
-import { ForumPage } from '../pages/forum/forum';
-import { CourseDetailPage } from '../pages/course-detail/course-detail';
-import { LecturerPage } from '../pages/lecturer/lecturer';
-import { StudentProfileEditPage } from '../pages/student-profile-edit/student-profile-edit';
-import { CoursesPage } from '../pages/courses/courses';
-import { NotificationsPage } from '../pages/notifications/notifications';
-import { LoginPage } from '../pages/login/login';
-import { RegistrationPage } from '../pages/registration/registration';
-import { ProfilePage } from '../pages/profile/profile';
 import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-service';
 import { UserProvider } from '../providers/user/user';
@@ -28,20 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ProfilePage,
-    RegistrationPage,
-    LoginPage,
-    NotificationsPage,
-    CoursesPage,
-    LecturerPage,
-    StudentProfileEditPage,
-    CourseDetailPage,
-    ForumPage,
-    AddForumPostPage,
-    RemoveForumPostPage,
-    VideosPage,
-    CreateQuizPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -52,19 +26,6 @@ import { HttpClientModule } from '@angular/common/http';
   entryComponents: [
     MyApp,
     HomePage,
-    ProfilePage,
-    RegistrationPage,
-    LoginPage,
-    NotificationsPage,
-    CoursesPage,
-    LecturerPage,
-    StudentProfileEditPage,
-    CourseDetailPage,
-    ForumPage,
-    AddForumPostPage,
-    RemoveForumPostPage,
-    VideosPage,
-    CreateQuizPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ProfilePage } from '../profile/profile';
-import { LoginPage } from '../login/login';
-import { NotificationsPage } from '../notifications/notifications';
-import { CoursesPage } from '../courses/courses';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
-import { ForumPage } from '../forum/forum';
-import { VideosPage } from '../videos/videos';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
   email: string;
@@ -18,41 +12,40 @@ export class HomePage {
 
   constructor(public navCtrl: NavController, public auth: AuthServiceProvider) {
     this.email = localStorage.getItem('myappEmail');
-    console.log("This is constructor...");
+    console.log('This is constructor...');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
-    this.auth.getUserProfile(this.email).subscribe((resp) => {
+    this.auth.getUserProfile(this.email).subscribe(resp => {
       console.log(resp[0]);
       this.userProfile = resp[0];
       localStorage.setItem('myAppUser', JSON.stringify(this.userProfile));
-    })
+    });
   }
 
-  goToProfile(){
-    this.navCtrl.push(ProfilePage);
+  goToProfile() {
+    this.navCtrl.push('ProfilePage');
   }
 
-  goToNotifications(){
-    this.navCtrl.push(NotificationsPage);
+  goToNotifications() {
+    this.navCtrl.push('NotificationsPage');
   }
 
-  goToCourses(){
-    this.navCtrl.push(CoursesPage);
+  goToCourses() {
+    this.navCtrl.push('CoursesPage');
   }
 
-  goToForum(){
-    this.navCtrl.push(ForumPage);
+  goToForum() {
+    this.navCtrl.push('ForumPage');
   }
 
-  goToVideos(){
-    this.navCtrl.push(VideosPage);
+  goToVideos() {
+    this.navCtrl.push('VideosPage');
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('myappEmail');
-    this.navCtrl.setRoot(LoginPage);
+    this.navCtrl.setRoot('LoginPage');
   }
-
 }
